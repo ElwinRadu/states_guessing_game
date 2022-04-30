ccad77a96d89f32cdebd31ba5f6038431857b30f
Łukasz Roczniak <flufferinek@gmail.com>
2022-04-30 17:57:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -15,13 +15,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
                                     prompt="What is another state's name? Type end to exit").title()
     if answer_state == "End":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -30,5 +28,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_xy.x), int(state_xy.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
-
-
